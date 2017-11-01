ad2ea719d89ece442dda478839f38ce3b2c9c956
Starside-Softworks <anthonyalampi@gmail.com>
2017-11-01 17:01:38 -0500

    Added comments
    Added comments to changes from last commit

--- x-factor/functions.php
+++ x-factor/functions.php
@@ -4,8 +4,7 @@ if (!function_exists('enqueue_styles')) {
 
     function enqueue_styles() {
    
-        wp_enqueue_style('my-main-style', get_stylesheet_uri(), false, '20150320');
-       /*wp_enqueue_style( 'style', get_stylesheet_uri() );*/
+        wp_enqueue_style('style', get_stylesheet_uri(), false, '20150320');
         }
     }
     add_action('wp_enqueue_scripts', 'enqueue_styles'); 
