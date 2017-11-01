c6680375dcc7dbc2477c8c97b8c6fb3190352b8f
richalex2010 <33013726+richalex2010@users.noreply.github.com>
2017-11-01 19:37:58 -0400

    pages

--- x-factor/header.php
+++ x-factor/header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes();?>>
 <head>
   <meta charset="<?php bloginfo('charset');?>" />
-  <title><?php wp_title(); ?></title>
+  <meta name="viewport" content="width=device-width">
+  <title><?php bloginfo('name');?></title>
   <link rel="pingback" href="<?php bloginfo('pingback_url');?>" />
   <?php wp_head();?>
 
--- x-factor/index.php
+++ x-factor/index.php
@@ -5,8 +5,6 @@ get_header();
   if(have_posts()):
     while(have_posts()) : the_post(); ?>
 
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
     <?php the_content(); ?>
 
   <?php endwhile;
